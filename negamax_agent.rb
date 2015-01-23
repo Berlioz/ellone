f499a7d3f56f0abcdf128ff3f9a9a234d4f737c3
Victor Fan <vfan@blurb.com>
2015-01-23 13:54:03 -0800

    A/B pruning, better user interface

--- negamax_agent.rb
+++ negamax_agent.rb
@@ -1,5 +1,5 @@
 class NegamaxAgent
-  def initialize(board, max_hand, min_hand)
+  def initialize(board, max_hand, min_hand, difficulty = 8)
     @base_board = board
     @max_hand = max_hand
     @min_hand = min_hand
@@ -8,18 +8,21 @@ class NegamaxAgent
     #  "will min or max make the last move", which due to the nature of the
     #  game can be derived from the number of remaining spaces when the agent
     #  is to make a move.
-    @polarity = board.open_spaces.count % 2 == 0 ? 1 : 0
-    @d = []
+    @polarity = board.open_spaces.count % 2 == 0 ? 1 : -1
+    @difficulty = difficulty
   end
 
   def invoke
-    negamax(@base_board, @max_hand, @min_hand, @polarity, turns, false)
+  	debug = @base_board.open_spaces.length == 1 ? true : false
+    negamax(@base_board, @max_hand, @min_hand, @polarity, turns, -100, 100, debug)
   end
 
+  # depth function
   def turns
-    @base_board.open_spaces.count > 7 ? 4 : 5
+    @base_board.open_spaces.count > 7 ? [2, @difficulty - 3].max : @difficulty
   end
 
+  #@return [card, [x, y]]
   def generate_moves(player_hand, spaces)
     if spaces.length == 9
       spaces = spaces.select{|x, y| x != 1 && y != 1}
@@ -29,8 +32,17 @@ class NegamaxAgent
     end
   end
 
+  # W E L P
+  def tiebreaker(candidates, board)
+  	candidates.first
+  end
+
   #@return [card, x, y, score]
-  def negamax(board, max_hand, min_hand, polarity, depth_left, debug=false)
+  def negamax(board, max_hand, min_hand, polarity, depth_left, alpha, beta, debug=false)
+  	#binding.pry if debug
+    skip = false
+    skipped_score = nil
+
     if depth_left == 0
       [nil, nil, nil, polarity * board.score(max_hand.first.color)]
     elsif board.open_spaces.length == 0
@@ -38,30 +50,28 @@ class NegamaxAgent
     else
       moves = generate_moves(max_hand, board.open_spaces)
       best_score_so_far = -100
-      best_move_so_far = nil
+      best_moves_so_far = []
       moves.each do |move|
       	card = move.first
       	x, y = move.last
       	future = board.next_state(card, x, y)
-        result = negamax(future, min_hand, max_hand - [card], polarity * -1, depth_left - 1)
+        result = negamax(future, min_hand, max_hand - [card], polarity * -1, depth_left - 1, -1 * beta, -1 * alpha)
         score = -1 * result.last
-        @d << [score, move] if debug
         if score > best_score_so_far
           best_score_so_far = score
-          best_move_so_far = move
+          best_moves_so_far = [move]
+        elsif score == best_score_so_far
+          best_moves_so_far << move
+        end
+
+        alpha = [alpha, best_score_so_far].max
+        if alpha >= beta
+          break
         end
       end
 
-      if debug
-        c = best_move_so_far.first
-        x = best_move_so_far.last.first
-        y = best_move_so_far.last.last
-        f = board.next_state(c, x, y)
-        h = max_hand - [c]
-        binding.pry
-      end
-
-      [best_move_so_far.first, best_move_so_far.last.first, best_move_so_far.last.last, best_score_so_far]
+      best_move = tiebreaker(best_moves_so_far, board)
+      [best_move.first, best_move.last.first, best_move.last.last, best_score_so_far]
     end
 
   end
--- test_game.rb
+++ test_game.rb
@@ -2,18 +2,49 @@ require 'pry'
 require './board.rb'
 require './negamax_agent.rb'
 
+$switch = false
+if ARGV.length == 2
+  Random.srand(ARGV.first.to_i)
+  $switch = true
+elsif ARGV.length == 1
+  Random.srand(ARGV.first.to_i)
+end
+
 class TestGame
-  def initialize
+  def initialize(hints = false, difficulty = 8)
     @board = Board.new
     @player_hand = generate_hand(1)
     @ai_hand = generate_hand(2)
+    @difficulty = difficulty
+    @hints = hints
+
+    if $switch
+    	puts "switching hands..."
+ 		@player_hand.each do |k, v|
+ 			v.color = 2
+ 		end
+ 		@ai_hand.each do |k, v|
+ 			v.color = 1
+ 		end
+ 		temp = @ai_hand
+ 		@ai_hand = @player_hand
+ 		@player_hand = temp
+    end
   end
 
   def run
   	0.upto(8).each do |turn|
-  	  turn % 2 == 0 ? player_move(turn) : ai_move(turn)
+  	  turn % 2 == ($switch ? 1 : 0) ? player_move(turn) : ai_move(turn)
     end
-    puts "Final score is: " + "#{@board.score}".colorize(@board.score >= 0 ? :green : :red)
+    puts "Final score is: " + "#{game_score(@board.score) < 0 ? '-' : '+'}#{game_score(@board.score)}".colorize(@board.score >= 0 ? :green : :red) + " (board score #{@board.score})"
+  end
+
+  def game_score(score)
+    if @player_hand.values.compact.length > 0
+      score + 1
+    else
+      score - 1
+    end
   end
 
   def generate_hand(c)
@@ -39,42 +70,23 @@ class TestGame
     puts "#{pc(cards[0], 0)} #{pc(cards[1], 0)} #{pc(cards[2], 0)} #{pc(cards[3], 0)} #{pc(cards[4], 0)}"
     puts "#{pc(cards[0], 1)} #{pc(cards[1], 1)} #{pc(cards[2], 1)} #{pc(cards[3], 1)} #{pc(cards[4], 1)}"
     puts "#{pc(cards[0], 2)} #{pc(cards[1], 2)} #{pc(cards[2], 2)} #{pc(cards[3], 2)} #{pc(cards[4], 2)}"
+    cards = @ai_hand.values
+    puts "AI HAND:"
+    puts "#{pc(cards[0], 0)} #{pc(cards[1], 0)} #{pc(cards[2], 0)} #{pc(cards[3], 0)} #{pc(cards[4], 0)}"
+    puts "#{pc(cards[0], 1)} #{pc(cards[1], 1)} #{pc(cards[2], 1)} #{pc(cards[3], 1)} #{pc(cards[4], 1)}"
+    puts "#{pc(cards[0], 2)} #{pc(cards[1], 2)} #{pc(cards[2], 2)} #{pc(cards[3], 2)} #{pc(cards[4], 2)}"
   end
 
   # print a player move which results in maximum score shift
-  def look_ahead(board, current_hand, ai_hand)
-    #n = NegamaxAgent.new(board, current_hand, ai_hand)
-    #n_suggested_move = n.invoke
-    #n_suggested_card = n_suggested_move.first
-    #n_x = n_suggested_move[1]
-    #n_y = n_suggested_move[2]
-    #n_s = n_suggested_move[3]
+  def display_hint(board, current_hand, ai_hand)
+    n = NegamaxAgent.new(board, current_hand, ai_hand)
+    n_suggested_move = n.invoke
+    n_suggested_card = n_suggested_move.first
+    n_x = n_suggested_move[1]
+    n_y = n_suggested_move[2]
+    n_s = n_suggested_move[3]
 
-    #puts "DEBUG: negamax suggested #{@player_hand.key(n_suggested_card)} @ #{n_x + 1}, #{n_y + 1} with score of #{n_s}"
-    #return
-
-    available_cards = current_hand.values.compact
-    available_spaces = board.open_spaces
-    possible_moves = available_cards.product(available_spaces)
-
-    best_moves = []
-    value_of_best_move = -10
-
-    possible_moves.each do |card, space|
-      x, y = space
-      value = board.next_state(card, x, y).score
-      if value > value_of_best_move
-        best_moves = [[card, x, y]]
-        value_of_best_move = value
-      elsif value == value_of_best_move
-      	best_moves << [card, x, y]
-      end
-    end
-
-    chosen_move = best_moves.sample
-    card, x, y = chosen_move
-
-    puts "HINT: placing #{current_hand.key(card).rstrip} at #{x+1}, #{y+1} gives a good score of #{value_of_best_move}"
+    puts "HINT: negamax suggested #{@player_hand.key(n_suggested_card)} @ #{n_x + 1}, #{n_y + 1} is no worse than your best move."
   end
 
   def determine_ai_move(board, ai_hand, player_hand)
@@ -84,7 +96,6 @@ class TestGame
     n_x = n_suggested_move[1]
     n_y = n_suggested_move[2]
     n_s = n_suggested_move[3]
-    puts "DEBUG: negamax suggested #{ai_hand.key(n_suggested_card)} @ #{n_x}, #{n_y} with score #{n_s}"
     return [n_suggested_card, n_x, n_y]
 
     available_cards = ai_hand.values.compact
@@ -131,33 +142,49 @@ class TestGame
     return value_of_best_move
   end
 
+  def get_input
+    puts "Input: card_letter x y (ex. c 2 3)\n"
+    input = $stdin.gets.gsub(',' , '').rstrip
+    letter, x, y = input.split
+    unless letter && x && y
+      puts "Malformed expression...\n"  
+      return nil
+    end
+    key = @player_hand.keys.detect{|k| k[0] == letter.upcase}
+    card = @player_hand[key]
+    unless @board.open_spaces.include?([x.to_i - 1, y.to_i - 1]) && card
+      puts "Illegal move...\n"
+      return nil
+    end
+    [card, x.to_i, y.to_i]
+  end
+
   def player_move(turn)
   	if turn == 0
   	  system "clear"
   	  display_game
   	end
+  	display_hint(@board, @player_hand.values.compact, @ai_hand.values.compact) if @hints
     
-    #look_ahead(@board, @player_hand)
-    #look_ahead(@board, @player_hand.values.compact, @ai_hand.values.compact)
-    puts "Input: card_letter x y (ex. c 2 3)\n"
-    input = gets.gsub(',' , '').rstrip
-    letter, x, y = input.split
-    key = @player_hand.keys.detect{|k| k[0] == letter.upcase}
-    card = @player_hand[key]
+    player_move = nil
+    while player_move.nil?
+      player_move = get_input
+    end
+    card, x, y = player_move
 
-    @board.make_move(card, x.to_i - 1, y.to_i - 1)
-    @player_hand[key] = nil 
+    @board.make_move(card, x - 1, y - 1)
+    @player_hand[@player_hand.key(card)] = nil 
 
     system "clear"
     display_game
     puts "#{@board.open_spaces.count} spaces left. Enter to continue..."
-    _ = gets
+    _ = $stdin.gets
   end
 
   def ai_move(turn)
-    system "clear"
-
+    puts "AI is thinking..."
     move = determine_ai_move(@board, @ai_hand, @player_hand)
+    system "clear"
     card, x, y = move
     @board.make_move(card, x, y)
     @ai_hand.delete(@ai_hand.key(card))
@@ -168,11 +195,30 @@ class TestGame
 end
 
 r = Rules.instance
-r.base = true
-r.plus = true
-r.same = true
-r.combo = false
-r.same_wall = false
+puts "Do you want to use the default rules? (Plus, Same, Combo). Y/N"
+default_rules = $stdin.gets.rstrip.upcase[0] == "Y"
+if default_rules  
+  r.base = true
+  r.plus = true
+  r.same = true
+  r.combo = true
+  r.same_wall = false
+else
+  r.base = true
+  puts "PLUS? Y/N"
+  r.plus = $stdin.gets.rstrip.upcase[0] == "Y"
+  puts "SAME? Y/N"
+  r.same = $stdin.gets.rstrip.upcase[0] == "Y"
+  puts "COMBO? Y/N"
+  r.combo = $stdin.gets.rstrip.upcase[0] == "Y"
+  r.same_wall = false
+end
 
-t = TestGame.new
+puts "Please enter difficulty/lookahead length (2-8 inclusive):"
+difficulty = $stdin.gets.rstrip.to_i
+difficulty = 2 if difficulty < 2
+difficulty = 8 if difficulty > 8
+puts "Do you want hints? Y/N"
+hints = $stdin.gets.rstrip.upcase[0] == "Y"
+t = TestGame.new(hints, difficulty)
 t.run
